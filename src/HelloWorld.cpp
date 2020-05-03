379038b4610a4cf7e5ed72bf28a6aa54cda8854e
yogeshseralia <yogeshseralia21@gmail.com>
2020-05-03 20:24:37 +0530

    First Commit

---
+++ src/HelloWorld.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Yogesh Seralia
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "utils.h"
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	print_hello();
+	return 0;
+}
---
+++ src/utils.h
@@ -0,0 +1,21 @@
+/*
+ * utils.h
+ *
+ *  Created on: 03-May-2020
+ *      Author: yogeshseralia
+ */
+
+#include<iostream>
+using namespace std;
+
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+
+void print_hello(){
+  cout << "Hello";
+}
+
+
+#endif /* UTILS_H_ */
